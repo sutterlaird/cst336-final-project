164e4fcd3f9a41592a8ca6cfbe3edfeab01457f2
Gerardo Lopez <gerlopez@csumb.edu>
2018-10-31 17:26:17 +0000

    meme fix

--- examples/memegen/database.php
+++ examples/memegen/database.php
@@ -2,7 +2,7 @@
 // connect to our mysql database server
 
 function getDatabaseConnection() {
-    if(true){
+    if($_SERVER['SERVER_NAME'] == "cst336-gerardolopez.c9users.io"){
         $host = "localhost";
         $username = "gerardolopez";
         // $password = "cst336";
@@ -13,7 +13,7 @@ function getDatabaseConnection() {
         $host = "us-cdbr-iron-east-01.cleardb.net";
         $username = "be1ef852f0d88";
         $password = "97aa5537";
-        $dbname = heroku_773c99b358a4f76; 
+        $dbname = "heroku_773c99b358a4f76"; 
     }
     
     // Create connection
@@ -25,5 +25,6 @@ function getDatabaseConnection() {
 
 
 // $dbConn = getDatabaseConnection();
+
 
 ?>
