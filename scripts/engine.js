9db8b3e829b2e2c22cf138226e11f9e0057f3256
Sutter Laird <slaird@csumb.edu>
2018-11-28 21:46:38 +0000

    #secondcommit

---
+++ scripts/engine.js
@@ -0,0 +1,11 @@
+function buildHomepage() {
+    clearPage();
+    
+    $("#contentArea").html("yo put some content in me");
+    alert("it happened");
+}
+
+
+function clearPage() {
+    $("#contentArea").html("");
+}
